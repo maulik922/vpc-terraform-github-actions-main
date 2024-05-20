eeec286125c5d13d1e272284be853833847f9155
maulik-r-eq <125969168+maulik-r-eq@users.noreply.github.com>
2024-05-20 20:25:23 +0530

    Update provider.tf

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -7,12 +7,12 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-remote-backend-s3"
+    bucket = "my-bk-9223"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
